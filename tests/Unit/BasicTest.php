b3860b411e48f1e0f069f1d87c4655b24b09730d
Isaac Sai <isaacsai030@gmail.com>
2021-06-07 15:48:50 +0000

    Add Basic Unit Test

---
+++ tests/Unit/BasicTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class BasicTest extends TestCase
+{
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
